10730765ef64b0a9d978d154eb74c3f38196ac33
Konstantin <konstantin.wuestefeld@tu-dortmund.de>
2022-07-18 20:28:15 +0200

    updating cvat_loader (incl. massive speed-up)

--- CVAT_utils/cvat_loader.py
+++ CVAT_utils/cvat_loader.py
@@ -1,10 +1,20 @@
+"""
+required libraries:
+    * matplotlib (pip install matplotlib)
+    * numpy (pip install numpy)
+    * torch (https://pytorch.org/get-started/locally/)
+    * xmltodict (pip install xmltodict)
+    * PIL (pip install Pillow)
+"""
+
 import numpy as np
 import os.path as osp
 import xmltodict
 import collections
+import torch
+import sys
 from matplotlib.path import Path
-import errno
-import os
+from PIL import Image, ImageDraw
 
 """
 Important functions:
@@ -12,7 +22,8 @@ Important functions:
     • getBoxesByFileName(fileName): return boxes for a certain fileName
     • getMaskByFileName(fileName): return the pixel mask for a certain fileName
 """
-class CVATLoader:
+
+class CVATLoaderUtil:
     def __init__(self, xmlPath):
         self.xmlPath = xmlPath
         self.height = 0
@@ -20,14 +31,26 @@ class CVATLoader:
         self.minId = -1
         self.maxId = -1
         self.annotationData = collections.OrderedDict()  # fileName -> [id, polygons, boxes, mask]
+        self.scaleTo = []
 
         if not osp.exists(xmlPath):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), xmlPath)
+            print("Error: Cannot find {}".format(xmlPath))
+            return
 
         self.loadAnnotations()
 
     def getFileNames(self):
         return list(self.annotationData.keys())
+
+    def getFirstFileName(self):
+        names = sorted(self.annotationData.keys())
+        if len(names) > 0:
+            return names[0]
+
+    def getLastFileName(self):
+        names = sorted(self.annotationData.keys())
+        if len(names) > 0:
+            return names[-1]
 
     def hasData(self):
         return len(self.annotationData) > 0
@@ -36,7 +59,23 @@ class CVATLoader:
         return fileName in self.annotationData
 
     def getPolygonsByImgName(self, fileName):
-        return self.annotationData[fileName][1]
+        polys = self.annotationData[fileName][1]
+        polys = [[ [pt[1], pt[0]] for pt in poly] for poly in polys]
+        return polys
+
+    def getBoxesByImgName(self, fileName):
+        polys = self.getPolygonsByImgName(fileName)
+        boxes = []
+
+        for poly in polys:
+            x_min = min([p[0] for p in poly])
+            x_max = max([p[0] for p in poly])
+            y_min = min([p[1] for p in poly])
+            y_max = max([p[1] for p in poly])
+            boxes.append([x_min, y_min, x_max, y_max])
+
+        boxes = torch.tensor(boxes)
+        return boxes
 
     def getShape(self, fileName):
         return (self.width, self.height)
@@ -46,72 +85,69 @@ class CVATLoader:
 
     def getMaskByFileName(self, fileName):
         return self.annotationData[fileName][3]
-        
+
     def loadAnnotations(self):
-        try:
-            with open(self.xmlPath) as fd:
-                doc = xmltodict.parse(fd.read())
+        with open(self.xmlPath) as fd:
+            doc = xmltodict.parse(fd.read())
 
-            imgInfo = []
+        imgInfo = []
 
-            for imgEntry in doc['annotations']['image']:
-                self.height = int(imgEntry['@height'])
-                self.width = int(imgEntry['@width'])
-                fileName = imgEntry['@name']
-                id = int(imgEntry['@id'])
+        for imgEntry in doc['annotations']['image']:
+            self.height = int(imgEntry['@height'])
+            self.width = int(imgEntry['@width'])
+            fileName = imgEntry['@name']
+            id = int(imgEntry['@id'])
 
-                if self.minId == -1:
-                    self.minId = id
-                    self.maxId = id
+            if self.minId == -1:
+                self.minId = id
+                self.maxId = id
+            else:
+                self.minId = min(id, self.minId)
+                self.maxId = max(id, self.maxId)
+
+            polygons = []
+            if 'polygon' in imgEntry:
+                polyEntries = imgEntry['polygon']
+
+                if '@label' in polyEntries:
+                    pointsString = polyEntries['@points']
+                    points = []
+
+                    for pointString in pointsString.split(';'):
+                        # store this point as a list of float values
+                        points.append(list(map(lambda e: float(e), pointString.split(','))))
+
+                    polygons.append(points)
                 else:
-                    self.minId = min(id, self.minId)
-                    self.maxId = max(id, self.maxId)
-
-                polygons = []
-                if 'polygon' in imgEntry:
-                    polyEntries = imgEntry['polygon']
-
-                    if '@label' in polyEntries:
-                        pointsString = polyEntries['@points']
+                    for polyEntry in polyEntries:
+                        pointsString = polyEntry['@points']
                         points = []
 
                         for pointString in pointsString.split(';'):
-                            # store this point as a list of float values
-                            points.append(list(map(lambda e: float(e), pointString.split(','))))
+                            # store this point as a list
+                            points.append(list(map(lambda e: int(float(e)), pointString.split(','))))
 
                         polygons.append(points)
-                    else:
-                        for polyEntry in polyEntries:
-                            pointsString = polyEntry['@points']
-                            points = []
+                    polygons.append(points)
 
-                            for pointString in pointsString.split(';'):
-                                # store this point as a list of float values
-                                points.append(list(map(lambda e: float(e), pointString.split(','))))
+            boxes = []
 
-                            polygons.append(points)
-                        polygons.append(points)
+            if len(polygons) > 0:
+                for poly in polygons:
+                    xPos = list(map(lambda points: points[0], poly))
+                    xPos.sort()
+                    yPos = list(map(lambda points: points[1], poly))
+                    yPos.sort()
+                    xMin, xMax = xPos[0], xPos[-1]
+                    yMin, yMax = yPos[0], yPos[-1]
+                    boxes.append([xMin, yMin, xMax, yMax])
 
-                boxes = []
+            mask = (maskImgFromPolygons(polygons, self.width, self.height) > sys.float_info.epsilon).astype(np.uint8)
+            mask = mask.transpose(1,0)
+            mask = torch.from_numpy(mask)
 
-                if len(polygons) > 0:
-                    for poly in polygons:
-                        xPos = list(map(lambda points: points[0], poly))
-                        xPos.sort()
-                        yPos = list(map(lambda points: points[1], poly))
-                        yPos.sort()
-                        xMin, xMax = xPos[0], xPos[-1]
-                        yMin, yMax = yPos[0], yPos[-1]
-                        boxes.append([xMin, yMin, xMax, yMax])
-
-                mask = maskImgFromPolygons(polygons, self.width, self.height)
-
-                assert len(polygons) == len(boxes)
-                self.annotationData[fileName] = [id, polygons, boxes, mask]
-
-        except:
-            print("Error: Exception during loading of {}".format(self.xmlPath))
-            return
+            assert len(polygons) == len(boxes)
+            self.annotationData[fileName] = [id, polygons, boxes, mask]
 
         if len(self.annotationData) == 0:
             print("Warning: There was no exception, but no fitting annotations were found in {}".format(self.xmlPath))
@@ -138,25 +174,46 @@ def maskImgFromPolygons(polygons, im_width, im_height):
 
 def maskFromPolygon(data, pixelValue=1, heightFirst=True):
     polygon, height, width = data
+    polygon = [list(e) for e in polygon]
+    polygon_x = [e[0] for e in polygon]
+    polygon_y = [e[1] for e in polygon]
+    polygon = list(zip(polygon_x, polygon_y))
 
-    polygon = list(map(lambda e: list(e), polygon))
+    img = Image.new('L', (height, width), 0)
+    ImageDraw.Draw(img).polygon(polygon, outline=1, fill=1)
+    mask = np.array(img)
 
-    if len(polygon) == 0:
-        if heightFirst:
-            return np.zeros(shape=(height, width), dtype=np.uint8)
-        else:
-            return np.zeros(shape=(width, height), dtype=np.uint8)
-
-    poly_path = Path(polygon)
-    x, y = np.mgrid[:height, :width]
-    coords = np.hstack((x.reshape(-1, 1), y.reshape(-1, 1)))
-    mask = poly_path.contains_points(coords)
-    mask = mask.reshape(height, width)
-
-    mask = mask.astype(np.uint8)
-    mask *= pixelValue
-
-    if not heightFirst:
+    if heightFirst:
         mask = mask.transpose(1, 0)
 
     return mask
+
+    #if len(polygon) == 0:
+    #    if heightFirst:
+    #        return np.zeros(shape=(height, width), dtype=np.uint8)
+    #    else:
+    #        return np.zeros(shape=(width, height), dtype=np.uint8)
+
+    #poly_path = Path(polygon)
+    #x, y = np.mgrid[:height, :width]
+    #coords = np.hstack((x.reshape(-1, 1), y.reshape(-1, 1)))
+    #mask = poly_path.contains_points(coords)
+    #mask = mask.reshape(height, width)
+
+    #mask = mask.astype(np.uint8)
+    #mask *= pixelValue
+
+    #if not heightFirst:
+    #    mask = mask.transpose(1, 0)
+
+    #return mask
+
+
+def scale(x, out_range=(-1, 1)):
+    domain = np.min(x), np.max(x)
+
+    if (domain[1] - domain[0]) == 0:
+        return x
+
+    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
+    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2
